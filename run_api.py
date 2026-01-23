715eb977a773c8d4f2e7f5964b7581787d7eddba
Canjico <pedro.damiao2906@gmail.com>
2026-01-22 22:15:53 -0300

    Delete run_api.py

--- run_api.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import uvicorn
-
-load_dotenv("keyazure.env")
-load_dotenv("keyadmin.env")
-
-if __name__ == "__main__":
-    uvicorn.run("API.main:app", host="127.0.0.1", port=8000, reload=True)
