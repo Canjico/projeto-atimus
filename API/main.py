32049e3bb6517a6d0923f5c338094f121a759762
Canjico <pedro.damiao2906@gmail.com>
2026-02-05 22:05:14 -0300

    Update main.py

--- API/main.py
+++ API/main.py
@@ -41,11 +41,8 @@ logger = logging.getLogger("api")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
-        "https://editais.atimus.agr.br"
+        "https://login.atimus.agr.br",   # frontend de login
+        "https://editais.atimus.agr.br", # frontend de editais
     ],
     allow_credentials=True,
     allow_methods=["*"],
